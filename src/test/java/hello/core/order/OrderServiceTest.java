d32082cc3129b4bee7ba181c3f5cdb8ea2ab436b
jinhyejoo <hjjin104@gmail.com>
2022-05-01 15:06:26 +0900

    Using @Test to checking codes

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        @Test
+        void crateOrder() {
+            /**
+             * long을 사용해도 되지만 이를 쓰면 null을 넣을 수 없음
+             * 이후에 DB 생성시 null을 사용할 수 있기 때문에 미리 고려
+             */
+
+            Long memberId = 1L;
+            Member member = new Member(memberId, "Jin", Grade.VIP);
+            memberService.join(member);
+
+            Order order = orderService.createOrder(memberId, "Apple", 3000);
+
+            Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        }
+}
